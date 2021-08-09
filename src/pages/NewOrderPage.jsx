593dfa1297d55a6bef5f42d541695cac7d2f22a2
ccrisolo <ccrisolo3@gmail.com>
2021-08-09 16:20:22 -0700

    saves progress

--- src/pages/NewOrderPage.jsx
+++ src/pages/NewOrderPage.jsx
@@ -2,16 +2,16 @@ import React, { useState, useEffect } from "react";
 
 const NewOrderPage = () => {
     const [menuItems, setMenuItems] = useState([]);
-
+    console.log('hitting new order page')
     // - Fetch the menuItems from the server via AJAX
     // - When the data comes back, call setMenuItems to save in state
-    useEffect(() => {
-        console.log("NewOrderPage rendered");
-    });
+   useEffect(() => {
+       console.log('New Order Page rendered');
+   })
 
     return (
         <>
-            <h1>NewOrderPage</h1>
+            <h1>New Order Page</h1>
             <button onClick={setMenuItems}>Trigger re-render</button>
         </>
     );
